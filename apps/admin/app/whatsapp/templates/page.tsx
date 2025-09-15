07745ae34c402b1983d88304b9468b6cc6d66d37
Pushpraj Dwivedi <pushprajdwivedi001@gmail.com>
2025-09-15 22:12:27 +0530

    Fix: add admin app as part of monorepo instead of submodule

---
+++ apps/admin/app/whatsapp/templates/page.tsx
@@ -0,0 +1,11 @@
+import { useQuery } from "@tanstack/react-query";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
+export default function TemplatesPage() {
+    return (
+    <>
+    hello moto
+    </>)
+}
+
